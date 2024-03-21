b705f1ecd27dd49e3077351a3ad77022624caffa
Friedrich Wilms <f.wilms89@web.de>
2024-03-21 19:21:08 +0100

    removed config.cfg, pass REPO_DIR to main.sh who passes it to ansible as env

--- debug/build_dockers.sh
+++ debug/build_dockers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd .. # <- whole repo will be the build context
-REPO_DIR=$(cat ./config.yml | grep "repo_dir: " | sed 's/repo_dir: //')
 echo "Start building docker image"
-docker build --build-arg REPO_DIR_ARG=$REPO_DIR -t virgin_machine -f debug/Dockerfile .
+docker build -t virgin_machine -f debug/Dockerfile .
